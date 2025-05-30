0e3b58868484531569da90a259e414aa3fc9c483
hnascimento <heinriknascimento@somultas.com>
2025-05-30 11:20:32 -0300

    Atualização do script ativar usuário

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/api", methods=["GET"])
 def home():
-    return "API está rodando!"
+    return jsonify({"mensagem": "API está rodando!"})
+
+@app.route("/teste", methods=["GET"])
+def teste():
+    return jsonify({"mensagem": "Estou funcionando!"})
+
+@app.route("/ativaruser", methods=["GET"])
+def teste():
+    usuario = request.args.get("usuario")
+    return jsonify({"mensagem": "Estou funcionando!"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
