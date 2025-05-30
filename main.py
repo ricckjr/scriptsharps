88035e05b682485a3d8aede50c7e628f4f930729
hnascimento <heinriknascimento@somultas.com>
2025-05-30 10:42:51 -0300

    Corrige requirements e garante / rota

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from flask import Flask, jsonify
-import teste  # importa o script de teste
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/executar", methods=["GET"])
-def executar_teste():
-    resultado = teste.executar()
-    return jsonify({"mensagem": resultado})
+@app.route("/")
+def home():
+    return "API está rodando!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
